a439c3afacc97e6b1255cbf89fb7d4b9e473761d
Max <ageev.maks2014@yandex.ru>
2017-07-09 13:03:49 +0500

    Run some statement with addBatch();

--- src/main/java/ru/fearofcode/database/Main.java
+++ src/main/java/ru/fearofcode/database/Main.java
@@ -25,7 +25,12 @@ public class Main {
                 Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
                 Statement statement = connection.createStatement();
         ) {
-            ResultSet rest = statement.executeQuery("SELECT * FROM animal");
+            statement.addBatch("INSERT INTO animal(name, dest) VALUE('batch1','dest');");
+            statement.addBatch("INSERT INTO animal(name, dest) VALUE('batch2','dest');");
+            statement.addBatch("INSERT INTO animal(name, dest) VALUE('batch3','dest');");
+
+            statement.executeBatch();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
