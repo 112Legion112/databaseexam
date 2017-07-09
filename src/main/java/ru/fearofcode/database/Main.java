da6c2fce8573d90f5f99905f6128f76b28034b89
Max <ageev.maks2014@yandex.ru>
2017-07-09 23:58:39 +0500

    Use sql request with value for add data.  (preparedStatement)

--- src/main/java/ru/fearofcode/database/Main.java
+++ src/main/java/ru/fearofcode/database/Main.java
@@ -2,7 +2,10 @@ package ru.fearofcode.database;
 
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.Calendar;
 
 /**
  * Created by Max on 7/8/2017.
@@ -12,28 +15,36 @@ public class Main {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
+    private static final String INSERT_NEW = "INSERT INTO dish VALUES(?,?,?,?,?,?,?);";
+    private static final String GET_ALL = "SELECT * FROM dish;";
+
+
     public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
         try {
             Driver driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
 
-        } catch (SQLException e){
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            preparedStatement = connection.prepareStatement(INSERT_NEW);
+            preparedStatement.setInt(1,2);
+            preparedStatement.setString(2, "Inserted title");
+            preparedStatement.setString(3, "Inserted dest");
+            preparedStatement.setFloat(4, 0.0f);
+            preparedStatement.setBoolean(5, true);
+            preparedStatement.setDate(6, new Date(Calendar.getInstance().getTimeInMillis()));
+            preparedStatement.setBlob(7, new FileInputStream("smile.png"));
+
+            preparedStatement.execute();
+
+
+
+
+        } catch (SQLException | FileNotFoundException e){
             e.printStackTrace();
         }
-
-        try (
-                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-                Statement statement = connection.createStatement();
-        ) {
-            statement.addBatch("INSERT INTO animal(name, dest) VALUE('batch1','dest');");
-            statement.addBatch("INSERT INTO animal(name, dest) VALUE('batch2','dest');");
-            statement.addBatch("INSERT INTO animal(name, dest) VALUE('batch3','dest');");
-
-            statement.executeBatch();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
     }
 }
